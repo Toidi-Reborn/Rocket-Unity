84eca57c9bf9c43edf0fb009bc0c5d60d9e0f87d
Prusko <prusko@thisisprusko.com>
2020-10-17 15:47:30 -0500

    start of 53

--- Assets/rocket.cs
+++ Assets/rocket.cs
@@ -13,7 +13,8 @@ public class rocket : MonoBehaviour
 
     Rigidbody rigidbody; //defines var as a Rigidbody
     AudioSource audioSource;
-
+    [SerializeField] float rcsThrust = 200f;
+    [SerializeField] float mainThrust = 50f;
 
     // Start is called before the first frame update
     void Start(){
@@ -27,25 +28,69 @@ public class rocket : MonoBehaviour
         Rotate();  
     }
 
-    private void Thrust(){
-        // thrust and rotate can happen same time so seperate if statement
+
+void OnCollisionEnter(Collision collision){
+
+    switch (collision.gameObject.tag) {
+
+        case "friend": 
+            //do nothing - its my best friend
+            break;
+
+
+        default:
+            break;
+            
+
+
+    }
+
+
+
+
+        /*
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            Debug.DrawRay(contact.point, contact.normal, Color.white);
+        }
+        if (collision.relativeVelocity.magnitude > 2)
+            audioSource.Play();
+        */
+    
+    }
+
+
+
+    private void Rotate(){
+        
+        rigidbody.freezeRotation = true;  // take manual control of rotation
+        
+        float rotationSpeed = rcsThrust * Time.deltaTime;
+
+
         if (Input.GetKey(KeyCode.A)) {   //if key is pressed
-            print("Rotate Left");
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationSpeed);
         }
         else if (Input.GetKey(KeyCode.D)) {
-            print("Rotate Right");
-            transform.Rotate(-Vector3.forward);
+            
+            transform.Rotate(-Vector3.forward * rotationSpeed);
 
         }
+
+        rigidbody.freezeRotation = false; // resume physics control of rotation
+
+
+
     }
 
     //private can only be called from our own code
-    private void Rotate() {
+    private void Thrust() {
+
+
         if (Input.GetKey(KeyCode.Space)) {
             print("thrust");
 
-            rigidbody.AddRelativeForce(Vector3.up);
+            rigidbody.AddRelativeForce(Vector3.up * mainThrust);
             if (!audioSource.isPlaying) {
             audioSource.Play();
             }
@@ -53,6 +98,7 @@ public class rocket : MonoBehaviour
         else {
             audioSource.Stop();
         }
+
     }
 
 
